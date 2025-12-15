5eb11befee76d92ae45d9f988e6cdd7e81d63b97
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-12-15 14:53:31 +0900

    Add GiocciIntegrationTest

--- apps/giocci_integration/lib/giocci_integration.ex
+++ apps/giocci_integration/lib/giocci_integration.ex
@@ -2,17 +2,4 @@ defmodule GiocciIntegration do
   @moduledoc """
   Documentation for `GiocciIntegration`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GiocciIntegration.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- apps/giocci_integration/test/giocci_integration_test.exs
+++ apps/giocci_integration/test/giocci_integration_test.exs
@@ -1,8 +1,19 @@
 defmodule GiocciIntegrationTest do
   use ExUnit.Case
-  doctest GiocciIntegration
 
-  test "greets the world" do
-    assert GiocciIntegration.hello() == :world
+  setup do
+    :ok = Application.put_env(:giocci_client, :client_name, "giocci_client")
+    {:ok, _} = Application.ensure_all_started(:giocci_client)
+
+    on_exit(fn ->
+      :ok = Application.delete_env(:giocci_client, :client_name)
+      :ok = Application.stop(:giocci_client)
+    end)
+
+    :ok
+  end
+
+  test "" do
+    assert {:error, _} = GiocciClient.register_client("non-existent relay")
   end
 end

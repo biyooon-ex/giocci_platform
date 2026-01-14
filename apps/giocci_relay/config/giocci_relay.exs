daccf62ae14afb7ace39b275675d574942144ca6
Ryota Kinukawa <kinukawa@tombo-works.com>
2026-01-14 14:26:22 +0900

    Update giocci_relay/Dockerfile

--- apps/giocci_relay/config/giocci_relay.exs
+++ apps/giocci_relay/config/giocci_relay.exs
@@ -1,5 +1,5 @@
 # This config is an example for Giocci Realy Docker image
 import Config
 
-config :giocci_relay, zenoh_config_file_path: "/root/giocci_relay/zenoh.json"
+config :giocci_relay, zenoh_config_file_path: "/app/zenoh.json"
 config :giocci_relay, relay_name: "giocci_relay"

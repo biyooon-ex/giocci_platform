9483545552c0e1eca29fbd8f83aa57c9e87ed25e
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2026-03-11 15:46:19 +0900

    Release v0.3.1

--- apps/giocci/mix.exs
+++ apps/giocci/mix.exs
@@ -4,7 +4,7 @@ defmodule Giocci.MixProject do
   def project do
     [
       app: :giocci,
-      version: "0.3.1-rc.2",
+      version: "0.3.1",
       build_path: "./_build",
       config_path: "./config/config.exs",
       deps_path: "./deps",

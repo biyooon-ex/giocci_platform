33f8050acafb766aacb58ecf1bac7a63704d78c4
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2026-03-27 10:57:28 +0200

    Refer VERSIONS to specify

--- apps/giocci_relay/mix.exs
+++ apps/giocci_relay/mix.exs
@@ -1,10 +1,24 @@
 defmodule GiocciRelay.MixProject do
   use Mix.Project
 
+  @versions_path Path.join(__DIR__, "VERSIONS")
+  @version (if File.exists?(@versions_path) do
+              @versions_path
+              |> File.read!()
+              |> String.split("\n")
+              |> Enum.find_value(fn
+                "PROJECT_VERSION=" <> version -> String.trim(version)
+                _ -> false
+              end) || raise("PROJECT_VERSION not found in VERSIONS")
+            else
+              # Fallback to dummy version for Dependabot compatibility
+              "0.1.0-dependabot"
+            end)
+
   def project do
     [
       app: :giocci_relay,
-      version: "0.3.1",
+      version: @version,
       build_path: "./_build",
       config_path: "./config/config.exs",
       deps_path: "./deps",
